fc896453d9a96c625f110f1f54b07dde84267bdd
Vince.Chen <toto4726702@163.com>
2013-05-22 23:16:03 -0700

    Project Source

---
+++ src/cn/edu/shiep/Person.java
@@ -0,0 +1,30 @@
+package cn.edu.shiep;
+
+public class Person {
+
+	private String name;
+	private String sex = "male";
+	private String id;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	
+}
---
+++ src/cn/edu/shiep/action/CalculateAction.java
@@ -0,0 +1,13 @@
+package cn.edu.shiep.action;
+
+public class CalculateAction {
+
+	public int add(int a,int b){
+		return a+b;
+	}
+	
+	public int sub(int a,int b){
+		return a-b;
+	}
+	
+}
